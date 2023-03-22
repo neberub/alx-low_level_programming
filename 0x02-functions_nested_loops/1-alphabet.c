288c97e46d4cb50eee94d82f67ac330ff46322b5
neberu <neberu2011@gmail.com>
2023-03-22 12:55:14 -0700

    The read me file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * Thisis to print_alphabet - print all alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char letters;
+
+	for (letters = 'a'; letters <= 'z'; letters++)
+		_putchar(letters);
+	_putchar('\n');
+}
+
